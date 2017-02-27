99c92544d7db19fea29be7a19aa925dca422c8a5
NightFeather <ss900328@gmail.com>
2017-02-28 01:26:08 +0800

    extract introduction as external plain text file

---
+++ src/components/content/introduction.vue
@@ -0,0 +1,25 @@
+<template>
+  <article id="intro" class="reigion">
+    <header>關於我</header>
+    <section>
+      <pre>{{ this.content }}</pre>
+    </section>
+  </article>
+</template>
+
+<script>
+import SelfIntro from '@/data/self-intro.txt'
+
+export default {
+  name: 'content-intro',
+  data: function () {
+    return {
+      'content': SelfIntro
+    }
+  }
+}
+</script>
+
+<style lang='scss'>
+  #intro { background-color: #a55; }
+</style>
