0d86a7275396936d8e0d53866e734785cd5903ce
NightFeather <ss900328@gmail.com>
2017-02-28 01:27:02 +0800

    Contact
    nothing special

---
+++ src/components/content/contact.vue
@@ -0,0 +1,18 @@
+<template>
+  <article id="contact" class="reigion">
+    <header>找我嗎</header>
+    <section>
+      blah
+    </section>
+  </article>
+</template>
+
+<script>
+  export default {
+    name: 'content-contact'
+  }
+</script>
+
+<style lang='scss'>
+  #contact { background-color: #55a; }
+</style>
