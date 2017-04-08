4642d2e2a659eaf84ae3e2491c8e732c13c2ea7f
NightFeather <ss900328@gmail.com>
2017-04-08 23:41:50 +0800

    String Truncate Helper

--- src/mixins/string-helper.js
+++ src/mixins/string-helper.js
@@ -2,6 +2,13 @@ export default {
   filters: {
     capitalize: function (string) {
       return string.charAt(0).toUpperCase() + string.slice(1)
+    },
+    truncate: function (str, length) {
+      if (str.length > length) {
+        return str.substring(0, length - 3) + ' ...'
+      } else {
+        return str
+      }
     }
   }
 }
